4a4b01844acbfd0b114066e7d48a229bb5619580
Insaram Alam Siddique <alaminsaram92@gmail.com>
2020-11-16 19:06:26 +0500

    JS Intervals & Local Storage Applied

--- counter.js
+++ counter.js
@@ -1,8 +1,12 @@
-let counter = 0;
+if(!localStorage.getItem('counter')){
+    localStorage.setItem('counter', 0);
+}
 function count(){
+    let counter = localStorage.getItem('counter');
     counter++;
-    // alert(counter);
+    alert(counter);
     document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter',counter)
 
     if (counter % 10 == 0){
         alert(`counter reaches ${counter}`)
@@ -11,6 +15,8 @@ function count(){
 
 document.addEventListener('DOMContentLoaded',function(){
     document.querySelector('button').onclick = count;
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+    setInterval(count, 10000);
 });
 
 
